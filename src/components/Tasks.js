1dd699506749389e0b20c064f9f45d9a49540723
HoChiPants <aust.max.s@gmail.com>
2020-05-22 21:30:21 -0600

    finished checkbox

---
+++ src/components/Tasks.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Checkbox} from '../components/Checkbox';
+
+export const Tasks = ()=>{
+    const tasks = [];
+    let projectName = '';
+    return (
+        <div className = "tasks" data-testid= "tasks">
+            <h2 data-testid="project-name">
+                {projectName}
+            </h2>
+            <ul className="task__list">
+                {tasks.map(task=>(
+                    <li key={'${task.id}'}>
+                        <Checkbox id = {task.id} />
+                        <span> {task.task}</span>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
--- src/components/layout/Content.js
+++ src/components/layout/Content.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Sidebar} from './Sidebar'
+import {Tasks} from '../Tasks';
 
 export const Content = () => (
     <section>
