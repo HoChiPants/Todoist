ce9c0c87fa167400a9806c6a87300496f48a44e0
HoChiPants <aust.max.s@gmail.com>
2020-05-27 21:44:43 -0600

    started on adding tasks

---
+++ src/components/AddTask.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {FaRegListAlt, FaRegCalnedarAlt} from 'react-icons/fa';
+import moment from 'moment';
+import {firebase} from '../../firebase';
+import {useSelectedProjectValue} from '../context';
+
+export const AddTask = ({showAddTaskMain = true, showShouldMain = false, showQuickAddTask, setShowQuickAddTask,})=>
+ {
+    //useState
+
+    //return ()
+ };
+
+
