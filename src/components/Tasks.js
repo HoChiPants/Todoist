1a75cde2d5bb64f8b642b8072c90094fbad97370
HoChiPants <aust.max.s@gmail.com>
2020-06-02 22:05:02 -0600

    added adding task with features

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import {Checkbox} from '../components/Checkbox';
 import {AddTask} from '../components/AddTask';
 import {useTasks} from '../hooks';
